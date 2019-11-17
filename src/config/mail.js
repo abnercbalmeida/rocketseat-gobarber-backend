b9849840caa670b947d8e6b639f2a61110ba812e
Abner <abnercbalmeida@gmail.com>
2019-11-17 13:46:51 -0300

    add nodemailer configuration; add email sending method

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '3a1a60f0e0e7ae',
+    pass: 'bb71577781d10c',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
